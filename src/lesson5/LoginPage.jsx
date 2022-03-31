ef84301f494f29e0310fc65cc9fca6ae68cb7368
ramonbezerra <ramon.bezerra@outlook.com>
2022-03-30 22:50:10 -0300

    example for login with realist API

--- src/lesson5/LoginPage.jsx
+++ src/lesson5/LoginPage.jsx
@@ -3,9 +3,10 @@ import { Formik, ErrorMessage, Field } from 'formik';
 import * as Yup from 'yup';
 import { authContext } from './AuthProvider';
 import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const LoginSchema = Yup.object().shape({
-  email: Yup.string().required('Required').email('Invalid'),
+  username: Yup.string().required('Required'), //.email('Invalid'),
   password: Yup.string().required('Required').min(4, 'Too short!').max(10, 'Too long!')
 });
 
@@ -13,23 +14,31 @@ const LoginPage = () => {
   const { login } = React.useContext(authContext);
   let navigate = useNavigate();
   let location = useLocation();
+  const url = `https://immense-sands-97611.herokuapp.com/api/v1/login`;
 
   let from = location.state?.from || "/";
 
   const handleSubmitting = (values, { setSubmitting, setStatus }) => {
     setStatus({ isValidating: true });
-    login().then(navigate(from, { replace: true }))
     setTimeout(() => {
       console.info(JSON.stringify(values, null, 2));
       setSubmitting(false); // iremos fazer modificações aqui
+      axios.post(url, values)
+      .then(data => {
+          login(data.data.token).then(navigate(from, { replace: true }))
+          console.log(data.data.token);
+        })
+      .catch(error => {
+        console.log(error);
+      });
       setStatus({ isValidating: false });
-    }, 400);
+    }, 2000);
   };
 
   return (
     <Formik
       validationSchema={LoginSchema}
-      initialValues={{ email: '', password: ''}}
+      initialValues={{ username: '', password: ''}}
       onSubmit={handleSubmitting}
     >
       {({
@@ -40,12 +49,12 @@ const LoginPage = () => {
         }) => (
         <form onSubmit={handleSubmit}>
           <label>
-            E-mail*:
-            <Field type="text" name="email"
+            Username*:
+            <Field type="text" name="username"
                    onBlur={handleBlur}
                    onChange={handleChange}/>
           </label>
-          <ErrorMessage name="email" className="error" component="span"/>
+          <ErrorMessage name="username" className="error" component="span"/>
           <br/><br/>
           <label>
             Password*:
--- src/lesson5/hooks/useAuth.js
+++ src/lesson5/hooks/useAuth.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const useAuth = () => {
     const [authed, setAuthed] = React.useState(false);
+    const [token, setToken] = React.useState('');
 
-    const login = () => new Promise(() => {
+    const login = (token) => new Promise(() => {
         setAuthed(true);
+        setToken(token);
         setTimeout(() => {
             console.log('login', authed);
         }, 2000);
@@ -12,12 +14,13 @@ const useAuth = () => {
 
     const logout = () => new Promise(() => {
         setAuthed(false);
+        setToken('');
         setTimeout(() => {
             console.log('login', authed);
         }, 2000);
     });
 
-    return { authed, login, logout };
+    return { authed, token, login, logout };
 }
 
 export default useAuth;
